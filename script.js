0a82b5749e4bd9813544418032d7f66b84c622f3
jonasw1404-arch <jonasw1404@gmail.com>
2025-12-05 17:13:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const startBox = document.getElementById('startBox');
 const missionContainer = document.getElementById('missionContainer');
 
 startButton.addEventListener('click', () => {
-  // Startbox ausblenden
   startBox.style.display = 'none';
-  
-  // Mission Container einblenden
   missionContainer.classList.remove('hidden');
 });
